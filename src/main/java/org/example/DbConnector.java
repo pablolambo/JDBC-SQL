81bbf65a70d7dbfeb92f18544813eaaebe6a75d4
Paweł Frankowski <92943200+pablolambo@users.noreply.github.com>
2023-02-12 11:08:47 +0100

    Update DbConnector.java

--- src/main/java/org/example/DbConnector.java
+++ src/main/java/org/example/DbConnector.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DbConnector {
     private static final String URL = "jdbc:postgresql://localhost:5432/jdbc_example";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Fajnie123123!";
+    private static final String PASSWORD = "admin123";
 
     public static Connection connect(){
         Connection connection = null;
